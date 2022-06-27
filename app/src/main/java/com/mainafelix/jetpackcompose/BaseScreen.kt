13fe92e34c3f43730cb2971433ce3d986d187f52
felixmaina <karani.maina2010@gmail.com>
2022-06-27 19:10:42 +0300

    pass the top Screen composable and the History screen composable into the base composable

--- app/src/main/java/com/mainafelix/jetpackcompose/BaseScreen.kt
+++ app/src/main/java/com/mainafelix/jetpackcompose/BaseScreen.kt
@@ -1,7 +1,12 @@
 package com.mainafelix.jetpackcompose
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 //composable functions start with a capital Letter
@@ -13,4 +18,10 @@ fun BaseScreen(
     converterViewModel: ConverterViewModel = viewModel()
 ){
     var  list  = converterViewModel.getConversion()
+    Column(modifier = modifier.padding(10.dp)) {
+        TopScreen()
+        Spacer(modifier =modifier.height(10.dp) )
+        HistoryScreen()
+        
+    }
 }
