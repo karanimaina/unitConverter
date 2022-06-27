acfed5f04f564536c05d687bb5637d342c87b066
felixmaina <karani.maina2010@gmail.com>
2022-06-27 21:14:39 +0300

    define state to assig drop down the textFielsd the sae width as the drop down

--- app/src/main/java/com/mainafelix/jetpackcompose/ConversionMenu.kt
+++ app/src/main/java/com/mainafelix/jetpackcompose/ConversionMenu.kt
@@ -20,8 +20,8 @@ import androidx.compose.ui.unit.toSize
 @Composable
 fun ConversionMenu(list: List<Conversion>,modifier: Modifier = Modifier){
     // define the states
-//    var displayText by remember { mutableStateOf("Select the conversion type")}
-    var textFieldSize by remember { mutableStateOf(Size.Zero)} //To assign dropdown the same with as the TextField
+    var displayText by remember { mutableStateOf("Select the conversion type")}
+//    var textFieldSize by remember { mutableStateOf(Size.Zero)} //To assign dropdown the same with as the TextField
     var expanded by remember { mutableStateOf(false)}
     val icon = if(expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown
     OutlinedTextField(value = displayText, onValueChange = {displayText = it}, textStyle = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold),
