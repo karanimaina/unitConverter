b44ac6097239658e9d9cfdaa9a1bcd5233ee17c6
felixmaina <karani.maina2010@gmail.com>
2022-06-28 12:38:32 +0300

    implement the methods int he converter repository

--- app/src/main/java/com/mainafelix/jetpackcompose/data/ConverterResultImplementation.kt
+++ app/src/main/java/com/mainafelix/jetpackcompose/data/ConverterResultImplementation.kt
@@ -1,4 +1,21 @@
 package com.mainafelix.jetpackcompose.data
 
-class ConverterResultImplementation :ConverterRepository{
+import kotlinx.coroutines.flow.Flow
+
+class ConverterResultImplementation(private val dao: ConverterDao) :ConverterRepository{
+    override suspend fun insertResult(result: ConversionResult) {
+      dao.insertResult(result)
+    }
+
+    override suspend fun deleteResult(result: ConversionResult) {
+   dao.deleteResult(result)
+    }
+
+    override suspend fun deleteAllResults(result: ConversionResult) {
+ dao.deleteAll()
+    }
+
+    override fun getSavedResults(): Flow<List<ConversionResult>> {
+   return dao.getResults()
+    }
 }
