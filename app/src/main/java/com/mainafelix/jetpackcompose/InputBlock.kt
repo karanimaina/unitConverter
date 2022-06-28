2bbd1f414ba4086a144e878f8dc7c049e5bc64ca
felixmaina <karani.maina2010@gmail.com>
2022-06-28 06:46:27 +0300

    set the outlined butto to aid i conversio

--- app/src/main/java/com/mainafelix/jetpackcompose/InputBlock.kt
+++ app/src/main/java/com/mainafelix/jetpackcompose/InputBlock.kt
@@ -1,19 +1,17 @@
 package com.mainafelix.jetpackcompose
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -21,9 +19,11 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun  InputBlock(
+    context: Context = LocalContext.current,
     conversion: Conversion,
     InputText: MutableState<String>,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    calculate:(String) ->Unit
 ){
 Column(modifier = modifier.padding(0.dp,20.dp,0.dp,0.dp)){
     Row(modifier = modifier.fillMaxWidth()) {
@@ -42,8 +42,24 @@ Column(modifier = modifier.padding(0.dp,20.dp,0.dp,0.dp)){
         )
         Text(text = conversion.convertFrom,
             fontSize=24.sp,
-        modifier = modifier.padding(10.dp,30.dp,0.dp,0.dp) .fillMaxWidth(0.35F)
+        modifier = modifier
+            .padding(10.dp, 30.dp, 0.dp, 0.dp)
+            .fillMaxWidth(0.35F)
         )
+    }
+    Spacer(modifier = modifier.height(20.dp))
+
+    OutlinedButton(onClick = {
+        if (InputText.value!= ""){
+            calculate(InputText.value)
+        }else {
+            Toast.makeText(context, "please, please enter your value",Toast.LENGTH_LONG).show()
+        }
+    }, modifier = modifier.fillMaxWidth(1F)) {
+        Text(text = "Convert", fontSize = 36.sp, fontWeight = FontWeight.Bold
+        , color = Color.Blue)
+
+
     }
 }
 
--- app/src/main/java/com/mainafelix/jetpackcompose/TopScreen.kt
+++ app/src/main/java/com/mainafelix/jetpackcompose/TopScreen.kt
@@ -5,7 +5,7 @@ import androidx.compose.runtime.*
 @Composable
 fun TopScreen(list: List<Conversion>) {
 // the TopScreen composable function  will hold other 3 composable function
- val selectedConversion:MutableState<Conversion?> = remember { mutableStateOf(null) }
+  val selectedConversion:MutableState<Conversion?> = remember { mutableStateOf(null) }
    val inputText:MutableState<String> =  remember { mutableStateOf("")}
     
     ConversionMenu(list = list){
