d639a50b07228846f7cbdcd3fab32ce872540a5b
felixmaina <karani.maina2010@gmail.com>
2022-06-28 22:59:42 +0300

    design a History List composable to display the individual item

--- app/src/main/java/com/mainafelix/jetpackcompose/composable/BaseScreen.kt
+++ app/src/main/java/com/mainafelix/jetpackcompose/composable/BaseScreen.kt
@@ -10,31 +10,43 @@
 //import androidx.compose.ui.unit.dp
 //import androidx.lifecycle.viewmodel.compose.viewModel
 //import com.mainafelix.jetpackcompose.data.ConverterViewModel
-//import com.mainafelix.jetpackcompose.data.VewModelFactory
-//
-////composable functions start with a capital Letter
-////you should not pass vewmodel intances to the compasable ,only the data
-//@Composable
-//fun BaseScreen(
-//    factory: VewModelFactory,
-//    modifier: Modifier =Modifier,
-//    //passing the viewModel as the parameter
-//    converterViewModel: ConverterViewModel = viewModel(factory= factory)
-//){
-//    var  list  = converterViewModel.getConversion()
-//    val historyList = converterViewModel.resultList.collectAsState(initial = emptyList() )
-//    Column(modifier = modifier.padding(10.dp)) {
-//        TopScreen(list){message1,message2 ->
-//            converterViewModel.addResult(message1,message2)
-//        }
-//
-//        Spacer(modifier =modifier.height(10.dp) )
-//        HistoryScreen(historyList,{item ->
-//            converterViewModel.removeReult(item)
-//        },
-//        {
-//           converterViewModel.clearAll()
-//        })
-//
-//    }
-//}
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.mainafelix.jetpackcompose.composable.HistoryScreen
+import com.mainafelix.jetpackcompose.composable.TopScreen
+import com.mainafelix.jetpackcompose.data.ConverterViewModel
+import com.mainafelix.jetpackcompose.data.VewModelFactory
+
+//composable functions start with a capital Letter
+//you should not pass vewmodel intances to the compasable ,only the data
+@Composable
+fun BaseScreen(
+    factory: VewModelFactory,
+    modifier: Modifier =Modifier,
+    //passing the viewModel as the parameter
+    converterViewModel: ConverterViewModel = viewModel(factory= factory)
+){
+    var  list  = converterViewModel.getConversion()
+    val historyList = converterViewModel.resultList.collectAsState(initial = emptyList() )
+    Column(modifier = modifier.padding(10.dp)) {
+        TopScreen(list){message1,message2 ->
+            converterViewModel.addResult(message1,message2)
+        }
+
+        Spacer(modifier =modifier.height(10.dp) )
+        HistoryScreen(historyList,{item ->
+            converterViewModel.removeReult(item)
+        },
+        {
+           converterViewModel.clearAll()
+        })
+
+    }
+}
--- app/src/main/java/com/mainafelix/jetpackcompose/composable/TopScreen.kt
+++ app/src/main/java/com/mainafelix/jetpackcompose/composable/TopScreen.kt
@@ -1,40 +1,40 @@
-//package com.mainafelix.jetpackcompose.composable
-//
-//import android.util.Log
-//import androidx.compose.runtime.*
-//import com.mainafelix.jetpackcompose.data.ConversionMenu
-//import java.math.RoundingMode
-//import java.text.DecimalFormat
-//
-//@Composable
-//fun TopScreen(list: List<Conversion>,
-//save:(String,String)-> Unit) {
-//// the TopScreen composable function  will hold other 3 composable function
-//  val selectedConversion:MutableState<Conversion?> = remember { mutableStateOf(null) }
-//   val inputText:MutableState<String> =  remember { mutableStateOf("")}
-//    val typedValue = remember {mutableStateOf("0.0")}
-//    ConversionMenu(list = list){
-//        selectedConversion.value = it
-//        typedValue.value= "0.0"
-//    }
-//    selectedConversion.value?.let {
-//       InputBlock(conversion = it, InputText = inputText){inputText ->
-//           Log.i("Tag","user typed $inputText")
-//           typedValue.value = inputText
-//       }
-//    }
-//    if (typedValue.value != "0.0"){
-//        val input = typedValue.value.toDouble()
-//        val multiply = selectedConversion.value!!.multiplyBy
-//        val result = input * multiply
-//        // round to 4 decimal places
-//        val  df = DecimalFormat ("#.####")
-//        //rouding down runds torwards  0
-//        df.roundingMode = RoundingMode.DOWN
-//        val  roundingResult = df.format(result)
-//        val message1 = "${typedValue.value}  ${selectedConversion.value!!.convertFrom}"
-//        val message2 ="$roundingResult ${selectedConversion.value!!.convertTo}"
-//         save(message1,message2)
-//        ResultBlock(message1 = message1, message2 =message2 )
-//    }
-//}
+package com.mainafelix.jetpackcompose.composable
+
+import android.util.Log
+import androidx.compose.runtime.*
+import com.mainafelix.jetpackcompose.data.ConversionMenu
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
+@Composable
+fun TopScreen(list: List<Conversion>,
+save:(String,String)-> Unit) {
+// the TopScreen composable function  will hold other 3 composable function
+  val selectedConversion:MutableState<Conversion?> = remember { mutableStateOf(null) }
+   val inputText:MutableState<String> =  remember { mutableStateOf("")}
+    val typedValue = remember {mutableStateOf("0.0")}
+    ConversionMenu(list = list){
+        selectedConversion.value = it
+        typedValue.value= "0.0"
+    }
+    selectedConversion.value?.let {
+       InputBlock(conversion = it, InputText = inputText){inputText ->
+           Log.i("Tag","user typed $inputText")
+           typedValue.value = inputText
+       }
+    }
+    if (typedValue.value != "0.0"){
+        val input = typedValue.value.toDouble()
+        val multiply = selectedConversion.value!!.multiplyBy
+        val result = input * multiply
+        // round to 4 decimal places
+        val  df = DecimalFormat ("#.####")
+        //rouding down runds torwards  0
+        df.roundingMode = RoundingMode.DOWN
+        val  roundingResult = df.format(result)
+        val message1 = "${typedValue.value}  ${selectedConversion.value!!.convertFrom}"
+        val message2 ="$roundingResult ${selectedConversion.value!!.convertTo}"
+         save(message1,message2)
+        ResultBlock(message1 = message1, message2 =message2 )
+    }
+}
