de4e6fe443a6c66e8a3190099b47bf9d3afd369d
felixmaina <karani.maina2010@gmail.com>
2022-06-28 11:49:41 +0300

    define the fuction to insert delete and get all the result

--- app/src/main/java/com/mainafelix/jetpackcompose/data/ConverterDao.kt
+++ app/src/main/java/com/mainafelix/jetpackcompose/data/ConverterDao.kt
@@ -1,5 +1,21 @@
 package com.mainafelix.jetpackcompose.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
 interface ConverterDao {
+    @Insert
+    suspend fun insertResult(result: ConversionResult)
+    @Delete
+    suspend fun deleteResult(result:ConversionResult)
+    @Query("DELETE FROM result")
+    suspend fun deleteAll()
+    fun getResults(): Flow<List<ConversionResult>>
+
 
 }
