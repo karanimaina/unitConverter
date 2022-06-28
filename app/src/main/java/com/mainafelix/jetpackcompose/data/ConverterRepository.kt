dba43a1d08d811066c587c694909a07577db2f14
felixmaina <karani.maina2010@gmail.com>
2022-06-28 12:14:00 +0300

    define the functions present in the dao i the iterface Converterrepository

--- app/src/main/java/com/mainafelix/jetpackcompose/data/ConverterRepository.kt
+++ app/src/main/java/com/mainafelix/jetpackcompose/data/ConverterRepository.kt
@@ -1,5 +1,10 @@
 package com.mainafelix.jetpackcompose.data
 
-interface ConverterRepository {
+import kotlinx.coroutines.flow.Flow
 
+interface ConverterRepository {
+    suspend fun insertResult(result: ConversionResult)
+    suspend fun deleteResult(result: ConversionResult)
+    suspend fun deleteAllResults(result: ConversionResult)
+    fun getSavedResults(): Flow<List<ConversionResult>>
 }
