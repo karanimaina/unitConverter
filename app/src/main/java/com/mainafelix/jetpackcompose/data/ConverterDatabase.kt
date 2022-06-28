5abd3fb5e324d41dada8e325793cb4b12b69259b
felixmaina <karani.maina2010@gmail.com>
2022-06-28 12:05:49 +0300

    create a single ton class of database

--- app/src/main/java/com/mainafelix/jetpackcompose/data/ConverterDatabase.kt
+++ app/src/main/java/com/mainafelix/jetpackcompose/data/ConverterDatabase.kt
@@ -1,4 +1,24 @@
 package com.mainafelix.jetpackcompose.data
 
-class ConverterDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+@Database(entities = [ConversionResult::class], exportSchema = false, version = 1)
+abstract class ConverterDatabase: RoomDatabase() {
+    @Volatile
+    private var  INSTANCE :ConverterDatabase? = null
+    fun getInstance(context: Context):ConverterDatabase{
+        synchronized(this){
+         var instance = INSTANCE
+            if (instance== null){
+            instance= Room.databaseBuilder(
+                    context.applicationContext,
+                    ConverterDatabase::class.java,
+                    "result_database"
+            ).build()
+        }
+        return instance
+        }
+    }
 }
