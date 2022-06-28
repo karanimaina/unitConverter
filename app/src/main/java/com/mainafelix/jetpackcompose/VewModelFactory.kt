b5a9d8ed995964ff0afb7fd5e463c46533c9c4f3
felixmaina <karani.maina2010@gmail.com>
2022-06-28 13:41:43 +0300

    override the oncreate method and return  the converter viewModel Factory

--- app/src/main/java/com/mainafelix/jetpackcompose/VewModelFactory.kt
+++ app/src/main/java/com/mainafelix/jetpackcompose/VewModelFactory.kt
@@ -1,7 +1,10 @@
 package com.mainafelix.jetpackcompose
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.mainafelix.jetpackcompose.data.ConverterRepository
 
 class VewModelFactory(private val  repository:ConverterRepository):ViewModelProvider.NewInstanceFactory(){
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =ConverterViewModel(repository) as T
+
 }
